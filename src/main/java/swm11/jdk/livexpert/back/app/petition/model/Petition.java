19e3d2cea9c6709b8b67856e83c591b968d9e0a5
MangKyu <whalsrb1226@naver.com>
2020-08-03 18:45:13 +0900

    강연 청원 모델 추가

---
+++ src/main/java/swm11/jdk/livexpert/back/app/petition/model/Petition.java
@@ -0,0 +1,31 @@
+package swm11.jdk.livexpert.back.app.petition.model;
+
+import lombok.Getter;
+import org.hibernate.annotations.DynamicUpdate;
+import swm11.jdk.livexpert.back.app.common.model.Common;
+import swm11.jdk.livexpert.back.app.user.model.User;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "petition")
+@Getter
+@DynamicUpdate
+public class Petition extends Common implements Serializable {
+
+    @ManyToOne
+    @JoinColumn(nullable = false, referencedColumnName = "id")
+    private User user;                                          // 최초 등록한 사람
+
+    @Column(nullable = false, length = 20)
+    private String title;                                       // 청원 제목
+
+    @ManyToMany
+    @JoinTable(name = "petition_likes", joinColumns = @JoinColumn(name = "petition_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id"))
+    private List<User> likes = new ArrayList<>();               // 추가로 청원을 신청한 사람
+
+}

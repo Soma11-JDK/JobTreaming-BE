15807b139d7bf72441645be3d549423105f00d76
MangKyu <whalsrb1226@naver.com>
2020-08-03 18:44:02 +0900

    수입 및 정산 모델 추가

---
+++ src/main/java/swm11/jdk/livexpert/back/app/payment/model/Payment.java
@@ -0,0 +1,40 @@
+package swm11.jdk.livexpert.back.app.payment.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import swm11.jdk.livexpert.back.app.common.model.Common;
+import swm11.jdk.livexpert.back.app.lecture.model.Lecture;
+import swm11.jdk.livexpert.back.app.user.model.User;
+import swm11.jdk.livexpert.back.enums.payment.PayStatus;
+import swm11.jdk.livexpert.back.enums.payment.PayType;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "payment")
+@Getter
+public class Payment extends Common implements Serializable {
+
+    @ManyToOne
+    @JoinColumn(nullable = false, referencedColumnName = "id")
+    private User user;                                        // 결제한 사람
+
+    @ManyToOne
+    @JoinColumn(nullable = false, referencedColumnName = "id")
+    private Lecture lecture;                                  // 결제한 강연
+
+    @Setter
+    @Column(nullable = false, length = 20)
+    @Enumerated(EnumType.STRING)
+    private PayStatus status;                                   // 결제 상태
+
+    @Setter
+    @Column(nullable = false, length = 20)
+    @Enumerated(EnumType.STRING)
+    private PayType type;                                       // 결제 종류
+
+    @Column(nullable = false)
+    private Integer price;                                      // 금액
+
+}

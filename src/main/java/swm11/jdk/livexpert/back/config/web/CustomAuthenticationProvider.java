aae1507c3a95069ab6e36f7060d0e3a87eb8288f
MangKyu <whalsrb1226@naver.com>
2020-08-09 13:00:57 +0900

    인증 관련 설정 추가

---
+++ src/main/java/swm11/jdk/livexpert/back/config/web/CustomAuthenticationProvider.java
@@ -0,0 +1,47 @@
+package swm11.jdk.livexpert.back.config.web;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import swm11.jdk.livexpert.back.app.user.model.MyUserDetails;
+
+import javax.annotation.Resource;
+
+@RequiredArgsConstructor
+@Log4j2
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+    @Resource(name = "userDetailsService")
+    private UserDetailsService userDetailsService;
+    @NonNull
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) authentication;
+        // AuthenticaionFilter에서 생성된 토큰으로부터 아이디와 비밀번호를 조회함
+        String userEmail = token.getName();
+        String userPw = (String) token.getCredentials();
+        // UserDetailsService를 통해 DB에서 아이디로 사용자 조회
+        MyUserDetails userDetails = (MyUserDetails) userDetailsService.loadUserByUsername(userEmail);
+        if (!passwordEncoder.matches(userPw, userDetails.getPassword())) {
+            throw new BadCredentialsException(userDetails.getUsername() + "Invalid password");
+        }
+
+        return new UsernamePasswordAuthenticationToken(userDetails, userPw, userDetails.getAuthorities());
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+
+}

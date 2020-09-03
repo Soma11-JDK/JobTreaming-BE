5981cf45d46712678bf1975be48778a27e2585ad
MangKyu <whalsrb1226@naver.com>
2020-09-03 16:28:39 +0900

    전문가 API 기능 구체화 및 권한 처리

--- src/main/java/swm11/jdk/jobtreaming/back/app/expert/controller/ExpertController.java
+++ src/main/java/swm11/jdk/jobtreaming/back/app/expert/controller/ExpertController.java
@@ -2,26 +2,42 @@ package swm11.jdk.jobtreaming.back.app.expert.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import swm11.jdk.jobtreaming.back.app.expert.model.Expert;
+import swm11.jdk.jobtreaming.back.app.expert.model.ExpertSpecification;
 import swm11.jdk.jobtreaming.back.app.expert.service.ExpertService;
+import swm11.jdk.jobtreaming.back.app.expert.service.ExpertSpecificationService;
+import swm11.jdk.jobtreaming.back.app.user.model.MyUserDetails;
+import swm11.jdk.jobtreaming.back.app.user.service.UserService;
 
-import javax.annotation.Resource;
+import java.util.List;
 
 @Api(description = "전문가 API")
 @RestController
 @RequestMapping("/expert")
+@AllArgsConstructor
 public class ExpertController {
 
-    @Resource(name = "expertService")
     private ExpertService expertService;
+    private ExpertSpecificationService expertSpecificationService;
+    private UserService userService;
 
     @ApiOperation("전문가 등록")
+    @PreAuthorize("hasRole('ROLE_USER')")
     @PostMapping(value = "/register")
-    public ResponseEntity register(@RequestBody Expert expert) {
-        return ResponseEntity.ok(expertService.save(expert));
+    public ResponseEntity register(@RequestBody Expert expert, @RequestBody List<ExpertSpecification> specificationList) {
+        MyUserDetails userDetails = (MyUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        userDetails.setExpert(expert);
+        userService.save(userDetails.getUser());
+
+        specificationList.forEach(s -> s.setExpert(userDetails.getExpert()));
+        expertSpecificationService.saveAll(specificationList);
+        return ResponseEntity.ok().build();
     }
 
     @ApiOperation("전체 전문가 목록 조회")
@@ -32,7 +48,6 @@ public class ExpertController {
     }
 
     @ApiOperation("특정 전문가 상세 조회")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping(value = "/{id}")
     public ResponseEntity findById(@PathVariable("id") Long id) {
         return expertService.findById(id)
@@ -41,17 +56,24 @@ public class ExpertController {
     }
 
     @ApiOperation("특정 전문가 수정")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping(value = "/modify")
-    public ResponseEntity modify(@RequestBody Expert expert) {
-        return ResponseEntity.ok(expertService.save(expert));
+    public ResponseEntity modify(@RequestBody Expert expert, @RequestBody List<ExpertSpecification> specificationList) {
+        MyUserDetails userDetails = (MyUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (userDetails.getExpert().getId().equals(expert.getId())) {
+            expertService.save(expert);
+            specificationList.forEach(s -> s.setExpert(userDetails.getExpert()));
+            expertSpecificationService.saveAll(specificationList);
+            return ResponseEntity.ok().build();
+
+        }
+        return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
     }
 
     @ApiOperation("특정 전문가 삭제")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
-    @GetMapping(value = "/delete/{id}")
-    public ResponseEntity delete(@PathVariable("id") Long id) {
-        expertService.delete(id);
+    @PostMapping(value = "/delete")
+    public ResponseEntity delete(@RequestBody Expert expert) {
+        expertService.delete(expert.getId());
         return ResponseEntity.ok().build();
     }
 

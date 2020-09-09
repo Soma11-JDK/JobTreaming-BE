904dbbaae8d711d0eecd256a5d2385b02ce11d95
MangKyu <whalsrb1226@naver.com>
2020-09-09 17:39:06 +0900

    전문가 존재 예외 처리 클래스 추가

---
+++ src/main/java/swm11/jdk/jobtreaming/back/exception/ExpertNotFoundException.java
@@ -0,0 +1,9 @@
+package swm11.jdk.jobtreaming.back.exception;
+
+public class ExpertNotFoundException extends RuntimeException {
+
+    public ExpertNotFoundException() {
+        super("Expert Not Found Exception");
+    }
+
+}

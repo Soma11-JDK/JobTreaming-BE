fc7c7e43ed1aab2eadc6eae64807d6ba03e13641
MangKyu <whalsrb1226@naver.com>
2020-09-19 18:24:33 +0900

    전문가 평가 데이터를 String으로 수정

--- src/main/java/swm11/jdk/jobtreaming/back/app/expert/model/ExpertEvaluation.java
+++ src/main/java/swm11/jdk/jobtreaming/back/app/expert/model/ExpertEvaluation.java
@@ -26,22 +26,7 @@ public class ExpertEvaluation extends Common implements Serializable {
     @JoinColumn(nullable = false, referencedColumnName = "id")
     private Expert expert;                                                // 전문가
 
-    @Column(nullable = false, columnDefinition = "BOOLEAN DEFAULT false")
-    private boolean isSelected1;                                            // 첫번째 항목 선택 여부
-
-    @Column(nullable = false, columnDefinition = "BOOLEAN DEFAULT false")
-    private boolean isSelected2;                                            // 두번째 항목 선택 여부
-
-    @Column(nullable = false, columnDefinition = "BOOLEAN DEFAULT false")
-    private boolean isSelected3;                                            // 세번째 항목 선택 여부
-
-    @Column(nullable = false, columnDefinition = "BOOLEAN DEFAULT false")
-    private boolean isSelected4;                                            // 네번째 항목 선택 여부
-
-    @Column(nullable = false, columnDefinition = "BOOLEAN DEFAULT false")
-    private boolean isSelected5;                                            // 다섯번째 항목 선택 여부
-
-    @Column(nullable = false, columnDefinition = "BOOLEAN DEFAULT false")
-    private boolean isSelected6;                                            // 여섯번째 항목 선택 여부
+    @Column(nullable = false)
+    private String evaluation = "";                                       // 평가 내용
 
 }

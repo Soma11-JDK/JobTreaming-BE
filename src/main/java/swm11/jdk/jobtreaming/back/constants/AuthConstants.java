69b15ebf0b06f2fb949b92f21f90b3f7b727aa5a
MangKyu <whalsrb1226@naver.com>
2020-09-29 09:45:08 +0900

    인증 관련 상수 추가

---
+++ src/main/java/swm11/jdk/jobtreaming/back/constants/AuthConstants.java
@@ -0,0 +1,10 @@
+package swm11.jdk.jobtreaming.back.constants;
+
+public final class AuthConstants {
+
+    public static final String AUTH_HEADER = "Authorization";
+    public static final String TOKEN_TYPE = "BEARER";
+
+    public static final String CLAIMS_EMAIL = "email";
+    public static final String CLAIMS_ROLE = "role";
+}

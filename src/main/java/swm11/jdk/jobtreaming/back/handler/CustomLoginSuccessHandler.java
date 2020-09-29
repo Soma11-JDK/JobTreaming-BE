21efec91ec7493ca5eb169d2a4c55ee473479ead
MangKyu <whalsrb1226@naver.com>
2020-09-29 09:46:06 +0900

    로그인 성공 처리 Stateless하게 변경

--- src/main/java/swm11/jdk/jobtreaming/back/handler/CustomLoginSuccessHandler.java
+++ src/main/java/swm11/jdk/jobtreaming/back/handler/CustomLoginSuccessHandler.java
@@ -2,8 +2,11 @@ package swm11.jdk.jobtreaming.back.handler;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import swm11.jdk.jobtreaming.back.app.user.model.MyUserDetails;
+import swm11.jdk.jobtreaming.back.app.user.model.User;
+import swm11.jdk.jobtreaming.back.constants.AuthConstants;
+import swm11.jdk.jobtreaming.back.utils.TokenUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,10 +17,9 @@ public class CustomLoginSuccessHandler extends SavedRequestAwareAuthenticationSu
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) {
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        //User user = ((MyUserDetails)authentication.getPrincipal()).getUser();
-        //String token = TokenUtils.generateJwtToken(user);
-        //response.addHeader("Authorization", "Bearer " + token);
+        User user = ((MyUserDetails) authentication.getPrincipal()).getUser();
+        String token = TokenUtils.generateJwtToken(user);
+        response.addHeader(AuthConstants.AUTH_HEADER, AuthConstants.TOKEN_TYPE + " " + token);
     }
 
 }

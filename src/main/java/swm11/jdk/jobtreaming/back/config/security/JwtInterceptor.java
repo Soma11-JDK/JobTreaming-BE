30a585023f0462ae07a6e36bec60faaa273fa527
MangKyu <whalsrb1226@naver.com>
2020-10-30 11:37:44 +0900

    토큰 설정 추가

--- src/main/java/swm11/jdk/jobtreaming/back/config/security/JwtInterceptor.java
+++ src/main/java/swm11/jdk/jobtreaming/back/config/security/JwtInterceptor.java
@@ -2,10 +2,16 @@ package swm11.jdk.jobtreaming.back.config.security;
 
 import io.jsonwebtoken.Claims;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.servlet.HandlerInterceptor;
+import swm11.jdk.jobtreaming.back.app.user.model.MyUserDetails;
 import swm11.jdk.jobtreaming.back.constants.AuthConstants;
 import swm11.jdk.jobtreaming.back.utils.TokenUtils;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -17,15 +23,20 @@ import static swm11.jdk.jobtreaming.back.constants.AuthConstants.CLAIMS_ROLE;
 @Log4j2
 public class JwtInterceptor implements HandlerInterceptor {
 
+    @Resource(name = "userDetailsService")
+    private UserDetailsService userDetailsService;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
         String header = request.getHeader(AuthConstants.AUTH_HEADER);
         if (header != null) {
+            header = header.split(" ")[1];
             Optional<Claims> optional = TokenUtils.isValidToken(header);
             if (optional.isPresent()) {
                 Claims claims = optional.get();
-                request.setAttribute(CLAIMS_EMAIL, TokenUtils.getEmailFromClaims(claims));
-                request.setAttribute(CLAIMS_ROLE, TokenUtils.getRoleFromClaims(claims));
+                MyUserDetails myUserDetails = (MyUserDetails) userDetailsService.loadUserByUsername(TokenUtils.getEmailFromClaims(claims));
+                Authentication authentication = new UsernamePasswordAuthenticationToken(myUserDetails, "", myUserDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
                 return true;
             }
         }

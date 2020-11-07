2485fba94251e713b058448208ec6c0ec4242e91
MangKyu <whalsrb1226@naver.com>
2020-11-07 23:10:35 +0900

    강연 접근 예외 추가

---
+++ src/main/java/swm11/jdk/jobtreaming/back/exception/InvalidLectureAccessException.java
@@ -0,0 +1,9 @@
+package swm11.jdk.jobtreaming.back.exception;
+
+public class InvalidLectureAccessException extends RuntimeException {
+
+    public InvalidLectureAccessException() {
+        super(" NotFoundException");
+    }
+
+}

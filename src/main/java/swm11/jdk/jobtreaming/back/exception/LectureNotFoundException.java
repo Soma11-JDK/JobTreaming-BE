9e52787b5369a5a2d3fd7ffcc7d937d54bd5c879
MangKyu <whalsrb1226@naver.com>
2020-11-12 17:31:32 +0900

    강연 예외 추가

---
+++ src/main/java/swm11/jdk/jobtreaming/back/exception/LectureNotFoundException.java
@@ -0,0 +1,9 @@
+package swm11.jdk.jobtreaming.back.exception;
+
+public class LectureNotFoundException extends RuntimeException {
+
+    public LectureNotFoundException() {
+        super("Lecture Not Found Exception");
+    }
+
+}

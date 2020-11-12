b88c13c56ee835de68c3aabcea44e54969e6dca6
MangKyu <whalsrb1226@naver.com>
2020-11-12 17:30:47 +0900

    강연 반환 모델 추가

---
+++ src/main/java/swm11/jdk/jobtreaming/back/app/lecture/model/LectureResponse.java
@@ -0,0 +1,13 @@
+package swm11.jdk.jobtreaming.back.app.lecture.model;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class LectureResponse {
+
+    private String name;
+    private boolean isExpert;
+
+}

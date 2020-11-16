c36150a0836757229d6ddd3aabefa22a09617156
MangKyu <whalsrb1226@naver.com>
2020-11-16 15:19:38 +0900

    강연 평점 API 버그 수정

--- src/main/java/swm11/jdk/jobtreaming/back/app/expert/controller/ExpertRatingController.java
+++ src/main/java/swm11/jdk/jobtreaming/back/app/expert/controller/ExpertRatingController.java
@@ -5,7 +5,10 @@ import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import swm11.jdk.jobtreaming.back.app.expert.model.ExpertRating;
 import swm11.jdk.jobtreaming.back.app.expert.service.ExpertRatingService;
 import swm11.jdk.jobtreaming.back.exception.ExpertNotFoundException;
@@ -19,7 +22,7 @@ public class ExpertRatingController {
     private ExpertRatingService expertRatingService;
 
     @ApiOperation("특정 전문가 평점 조회")
-    @PostMapping(value = "/{id}")
+    @GetMapping(value = "/{id}")
     public ResponseEntity findById(@PathVariable("id") Long id) {
         ExpertRating expertRating = expertRatingService.findById(id).orElseThrow(ExpertNotFoundException::new);
         return ResponseEntity.ok(expertRating);
